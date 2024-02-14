fc9aa8d425abb344b515899b6de62cf3755201be
Овчинников <156998038+Ovchinnikov01@users.noreply.github.com>
2024-02-14 11:34:48 +0300

    Добавил php файлы

---
+++ controllers/head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+echo '<link rel="stylesheet" href="style/style.css">';
+echo '<link rel="stylesheet" href="stylesheet.css">';
+echo '<meta charset="UTF-8">';
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+echo '<link rel="preconnect" href="https://fonts.googleapis.com">';
+echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
+echo '<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;500;600;700;800;900&family=Unbounded:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">';
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php require_once 'controllers/head.php'; ?>
+</head>
+<body>
+    <?php require_once 'templates/container.php'; ?>
+    <?php require_once 'templates/catalog.php'; ?>
+</body>
+</html>
