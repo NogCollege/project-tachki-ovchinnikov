f1540d47df69ed6ff39b1cbb0375d6021a6b5367
Овчинников <156998038+Ovchinnikov01@users.noreply.github.com>
2024-02-16 01:13:24 +0300

    База данных

--- templates/catalog.php
+++ templates/catalog.php
@@ -1,72 +1,24 @@
-<div class="catalog">
-            <div class="content4">
-               <div class="grid1"><img class="car1" src="./img/image 22.png" alt="">
-                <img class="m-img" src=".templates/img/Frame 43.png" alt="">
-                 <h2 class="text-car1">BMW X2, 2021</h2>
-                 <img class="center1" src="./img/Frame 41.png" alt="">
-                 <hr class="hr2"><br>
-                 <a  class="bth-car1" href="#">Забронировать</a>
-                 <p class="arenda-car1">от 7 150 руб/сут.</p></div>
-                 <div class="grid1"><img class="car1" src="./img/image 22 (1).png" alt="">
-                    <img class="m-img" src="./img/Frame 43 (1).png" alt="">
-                    <h2 class="text-car1">Kia Sportage, 2022</h2>
-                    <img class="center1" src="./img/Frame 41.png" alt="">
-                    <hr class="hr2"><br>
-                    <a  class="bth-car1" href="#">Забронировать</a>
-                    <p class="arenda-car1">от 7 150 руб/сут.</p></div>
-                    <div class="grid1"><img class="car1" src="./img/image 22 (2).png" alt="">
-                        <img class="m-img" src="./img/Frame 43.png" alt="">
-                        <h2 class="text-car1">Lexus IS, 2018</h2>
-                        <img class="center1" src="./img/Frame 41.png" alt="">
-                        <hr class="hr2"><br>
-                        <a  class="bth-car1" href="#">Забронировать</a>
-                        <p class="arenda-car1">от 7 150 руб/сут.</p></div>
-        </div>
-        <div class="content4">
-            <div class="grid1"><img class="car1" src="./img/image 22 (3).png" alt="">
-                <img class="m-img" src="./img/Frame 43 (1).png" alt="">
-              <h2 class="text-car1">Jaguar XF,  2012</h2>
-              <img class="center1" src="./img/Frame 41.png" alt="">
-              <hr class="hr2"><br>
-              <a  class="bth-car1" href="#">Забронировать</a>
-              <p class="arenda-car1">от 7 150 руб/сут.</p></div>
-              <div class="grid1"><img class="car1" src="./img/image 22 (4).png" alt="">
-                <img class="d-img" src="./img/Frame 43 (1).png" alt="">
-                 <h2 class="text-car1">Toyota RAV4,  2017</h2>
-                 <img class="center1" src="./img/Frame 41.png" alt="">
-                 <hr class="hr2"><br>
-                 <a  class="bth-car1" href="#">Забронировать</a>
-                 <p class="arenda-car1">от 7 150 руб/сут.</p></div>
-                 <div class="grid1"><img class="car1" src="./img/image 22 (2).png" alt="">
-                    <img class="m-img" src="./img/Frame 43.png" alt="">
-                     <h2 class="text-car1">2021 Mazda 6, 2021</h2>
-                     <img class="center1" src="./img/Frame 41.png" alt="">
-                     <hr class="hr2"><br>
-                     <a  class="bth-car1" href="#">Забронировать</a>
-                     <p class="arenda-car1">от 7 150 руб/сут.</p></div>
+<?php
+require_once 'controllers/connect.php';
 
-    </div>
-    <div class="content4">
-        <div class="grid1"><img class="car1" src="./img/image 22 (5).png" alt="">
-            <img class="m-img" src="./img/Frame 43.png" alt="">
-          <h2 class="text-car1">Kia Sportage, 2022</h2>
-          <img class="center1" src="./img/Frame 41.png" alt="">
-          <hr class="hr2"><br>
-          <a  class="bth-car1" href="#">Забронировать</a>
-          <p class="arenda-car1">от 7 150 руб/сут.</p></div>
-          <div class="grid1"><img class="car1" src="./img/image 22 (6).png" alt="">
-            <img class="m-img" src="./img/Frame 43.png" alt="">
-             <h2 class="text-car1">Lexus IS, 2018</h2>
-             <img class="center1" src="./img/Frame 41.png" alt="">
-             <hr class="hr2"><br>
-             <a  class="bth-car1" href="#">Забронировать</a>
-             <p class="arenda-car1">от 7 150 руб/сут.</p></div>
-             <div class="grid1"><img class="car1" src="./img/image 22 (7).png" alt="">
-                <img class="m-img" src="./img/Frame 43.png" alt="">
-                 <h2 class="text-car1">BMW X2, 2021</h2>
-                 <img class="center1" src="./img/Frame 41.png" alt="">
-                 <hr class="hr2"><br>
-                 <a  class="bth-car1" href="#">Забронировать</a>
-                 <p class="arenda-car1">от 7 150 руб/сут.</p></div>
-    </div>
-</div>
+
+$sql = "SELECT * FROM catalog";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+   
+    while ($row = $result->fetch_assoc()) {
+        echo '<div class="car-card">';
+        echo '<img src="car_image.jpg" alt="Car Image">'; 
+        echo '<h3>' . $row['brand'] . ' ' . $row['model'] . '</h3>';
+        echo '<p>Цена: ' . $row['price'] . '</p>';
+       
+        echo '</div>';
+    }
+} else {
+    echo "Нет данных для отображения.";
+}
+
+
+$conn->close();
+?>
